593e38b998879caca0a26333af6b852d392cbe41
akourtis98 <akourtisdev@gmail.com>
2019-04-04 17:46:39 +0200

    added features... has errors

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ ObjectId = require('mongodb').ObjectID;
 
 const app = express();
 
+let day; // this will hold the days passed working towards your goal
+
 app.use(bodyParser.json());
 app.use(
     bodyParser.urlencoded({
@@ -112,10 +114,12 @@ app.post('/add/goal', verifyToken, (req,res,next)=>{
 
     const goal = {
         title: inp.title,
-        desc: inp.desc
+        desc: inp.desc,
+        user_id: inp.user_id,
+        goal_id: inp.goal_id
     };
 
-    db.getDB().collection("goals").insertOne(goal,(err,result)=>{
+    db.getDB().collection("Goals").insertOne(goal,(err,result)=>{
         if(err){
             const error = new Error("Failed");
             error.status = 400;
@@ -144,11 +148,157 @@ app.get('/get/goals', verifyToken, (req, res) => {
     });
 });
 
-const updateTime = () => {};
 
-const addRating = () => {};
+// insert new row in daily progress
+app.post('/add/daily-progress', verifyToken, (req,res,next)=>{
+    // Document to be inserted
+    const inp = req.body;
 
-const updateUserProgress = () => {};
+    const daily = {
+        rating: inp.rating,
+        desc: inp.desc,
+        user_id: inp.user_id,
+        goal_id: inp.goal_id,
+        day: getOnlyDayFromDateData()
+    };
+
+    db.getDB().collection("DailyProgress").insertOne(daily,(err,result)=>{
+        if(err){
+            const error = new Error("Failed");
+            error.status = 400;
+            next(error);
+        }
+        else
+            res.json({msg : "Successful!!!", error : null, inserted: daily});
+    });
+});
+
+// insert new row in daily progress
+app.post('/add/general-progress', verifyToken, (req,res,next)=>{
+    // Document to be inserted
+    const inp = req.body;
+
+    const daily = {
+        rating: inp.rating,
+        title: inp.title,
+        user_id: inp.user_id,
+        goal_id: inp.goal_id,
+        day: getCurrentDayOfGoal()
+    };
+
+    db.getDB().collection("DailyProgress").insertOne(daily,(err,result)=>{
+        if(err){
+            const error = new Error("Failed");
+            error.status = 400;
+            next(error);
+        }
+        else
+            res.json({msg : "Successful!!!", error : null, inserted: daily});
+    });
+});
+
+// to edit desc and rating of work session
+const updateDailyProgress = () => {
+   //  find daily by user_id
+   //  make new object with new data
+   //  and replace old one with it
+   //  make a new goal
+   //  app.post('/add/daily-progress', verifyToken, (req,res,next)=>{
+   //      // Document to be inserted
+   //      const inp = req.body;
+
+   //      const daily = {
+   //          rating: inp.rating,
+   //          desc: inp.desc,
+   //          user_id: inp.user_id,
+   //          goal_id: inp.goal_id,
+   //          day: getOnlyDayFromDateData()
+   //      };
+
+   //      db.getDB().collection("DailyProgress").insertOne(daily,(err,result)=>{
+   //          if(err){
+   //              const error = new Error("Failed");
+   //              error.status = 400;
+   //              next(error);
+   //          }
+   //          else
+   //              res.json({msg : "Successful!!!", error : null, inserted: daily});
+   //      });
+   // });
+};
+
+// for when a day passes by
+const deleteDailyProgress = () => {
+    // reset daily
+    // for when user loses, their data gets reset
+    // delete all records from general progress collection
+    // get all records from general progress table
+    app.get('/delete/daily-progress', verifyToken, (req, res) => {
+        jwt.verify(req.token, 'secretkey', (err, authData) => {
+            if(err) {
+                res.end(err);
+                res.sendStatus(403);
+            } else {
+                db.getDB().collection("DailyProgress").deleteMany({});
+            };
+        });
+    });
+
+};
+
+// for when user loses, their data gets reset
+// delete all records from general progress collection
+// get all records from general progress table
+app.get('/delete/general-progress', verifyToken, (req, res) => {
+    jwt.verify(req.token, 'secretkey', (err, authData) => {
+        if(err) {
+            res.end(err);
+            res.sendStatus(403);
+        } else {
+            db.getDB().collection("GeneralProgress").deleteMany({});
+        };
+    });
+});
+
+// get all records from general progress table
+app.get('/get/general-progress', verifyToken, (req, res) => {
+    jwt.verify(req.token, 'secretkey', (err, authData) => {
+        if(err) {
+            res.end(err);
+            res.sendStatus(403);
+        } else {
+            db.getDB().collection("GeneralProgress").find({}).toArray((err,goal)=>{
+                if(err)
+                    console.log(err);
+                else{
+                    res.json(goals);
+                }
+            });
+        }
+    });
+});
+
+const updateGeneralProgress = () => {
+    // take from daily progress and insert it to general and reset daily
+};
+
+const getDateData = () => {
+    return new Date(year, month, day);
+};
+
+const getOnlyDayFromDateData = () => {
+    return new Date(day);
+};
+
+const setCurrentDayOfGoal = () => {
+    day =+ 1;
+};
+
+// 24h counter
+const hoursCounter = () => {
+    // once it returns true (which is every 24 hours)
+    // once it completes call setCurrentDayOfGoal()
+};
 
 const regX = () => {};
 
